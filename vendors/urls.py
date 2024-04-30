4ac8ef171249d850d47fa28457400f1b4b3d4269
Devyanshi Srivastava <devyanshi2106@gmail.com>
2024-05-01 00:47:15 +0530

    Profile Management

--- vendors/urls.py
+++ vendors/urls.py
@@ -1,6 +1,7 @@
-from .views import VendorAPIView
+from .views import VendorAPIView, VendorUpdateDeleteRetrieveAPIView
 from django.urls import path
 
 urlpatterns = [
     path('vendors/', VendorAPIView.as_view(), name='vendors'),
+    path('vendors/<int:vendor_id>/', VendorUpdateDeleteRetrieveAPIView.as_view(), name='vendor-update-delete-retrieve'),
 ]
--- vendors/views.py
+++ vendors/views.py
@@ -3,6 +3,7 @@ from .models import Vendor
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 class VendorAPIView(APIView):
     def get(self, request):
@@ -24,5 +25,32 @@ class VendorAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
+class VendorUpdateDeleteRetrieveAPIView(APIView):
+    def get_object(self, vendor_id):
+        try:
+            return Vendor.objects.get(id=vendor_id)
+        except Vendor.DoesNotExist:
+            raise NotFound('Vendor not found')
+        
+
+    def get(self, request, vendor_id):
+        vendor = self.get_object(vendor_id)
+        serializer = VendorSerializer(vendor)
+        return Response(serializer.data)
+    
+    def put(self, request, vendor_id):
+        vendor = self.get_object(vendor_id)
+        serializer = VendorSerializer(vendor, data=request.data, partial = True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, vendor_id):
+        vendor = self.get_object(vendor_id)
+        vendor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
